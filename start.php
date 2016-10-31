546773208e75240eea2752c219affb5118a8ec79
walkor <walkor@workerman.net>
2016-10-31 23:26:13 +0800

    composer

--- start.php
+++ start.php
@@ -1,32 +1,52 @@
-<?php
+<?php 
 /**
- * run with command 
- * php start.php start
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link http://www.workerman.net/
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
+use \Workerman\Worker;
+use \Workerman\WebServer;
 
-ini_set('display_errors', 'on');
-use Workerman\Worker;
+// 自动加载类
+require_once __DIR__ . '/vendor/autoload.php';
 
-// 检查扩展
-if(!extension_loaded('pcntl'))
+
+$first_frame = '';
+$current_frame = '';
+$last_byte = '';
+$ws_worker = new Worker('Websocket://0.0.0.0:8124');
+$ws_worker->onWorkerStart = function($ws_worker)
 {
-    exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
-}
-
-if(!extension_loaded('posix'))
+    $connect_worker = new Worker('Websocket://0.0.0.0:8125');
+    $connect_worker->listen();
+    $connect_worker->onMessage = function(){};
+    $ws_worker->ConnectWorker = $connect_worker;
+};
+$ws_worker->onMessage = function($connection, $data) use ($ws_worker)
 {
-    exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
-}
+   foreach($ws_worker->ConnectWorker->connections as $client_connection)
+   {
+        $client_connection->send($data);
+   } 
+};
 
-// 标记是全局启动
-define('GLOBAL_START', 1);
+// WebServer
+$web = new WebServer("http://0.0.0.0:8123");
+// WebServer数量
+$web->count = 2;
+// 设置站点根目录
+$web->addRoot('www.your_domain.com', __DIR__.'/Web');
 
-require_once __DIR__ . '/Workerman/Autoloader.php';
 
-// 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+// 如果不是在根目录启动，则运行runAll方法
+if(!defined('GLOBAL_START'))
 {
-    require_once $start_file;
+    Worker::runAll();
 }
-// 运行所有服务
-Worker::runAll();
